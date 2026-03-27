970b06fab77ffc07551dcea4b51fa68ce4b571fd
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 14:15:04 +0545

    authValidator pushed

---
+++ backend/src/validators/authValidator.ts
@@ -0,0 +1,15 @@
+import Joi from "joi"
+
+export const registerSchema = Joi.object({
+    name: Joi.string().max(20).trim().required(),
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().min(8).required(),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+        "any.only": "Passwords do not match"
+    }),
+});
+
+export const loginSchema = Joi.object({
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().min(8).required(),
+})
