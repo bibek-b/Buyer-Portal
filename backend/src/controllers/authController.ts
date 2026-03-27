a2f18fece3b448e9ed72abfd6703128a5a75e27c
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 10:51:07 +0545

    authController pushed

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from "express";
+import User from "../models/User.js";
+import * as bcrypt from 'bcrypt-ts';
+
+export const register = async (req: Request, res: Response) => {
+    const { name, email, password, role } = req.body;
+
+    try {
+        const isUserExists = await User.findOne({email});
+
+        if(isUserExists){
+            return res.status(409).json({success: false, message: "User already exists! Please login"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+       const user = await User.create({name, email, password: hashedPassword, role});
+
+        return res.status(200).json({success: true, message: "Registration successful!" , data: user});
+        
+    } catch (error) {
+        return res.status(500).json({success:false, message: "Server error, Please try again."})
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    try {
+          const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({success: false, message: "User not found! Please register"});
+        }
+
+        const {password: userPassword, ...otherUserInfo} = user;
+
+        const isPasswordMatch = await bcrypt.compare(password, userPassword);
+
+        if(!isPasswordMatch){
+             return res.status(400).json({success: false, message: "Invalid password!"});
+        }
+
+        return res.status(200).json({success: true, message: "Login successful", data: user})
+        
+    } catch (error) {
+        return res.status(500).json({success:false, message: "Server error, Please try again."})
+    }
+}
