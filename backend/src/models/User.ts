e8baf82caa56d99d7db5e324a521bce51bafb28a
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 10:19:12 +0545

    user model created

---
+++ backend/src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    name: {
+    type: String,
+    required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+     password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['buyer'],
+        default: 'buyer',
+        required: true
+    }
+});
+
+export default mongoose.model('User', userSchema);
