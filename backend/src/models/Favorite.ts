1e964664d2212979cf4c1fab8424e35356ebcba9
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 10:26:58 +0545

    favorite model pushed

---
+++ backend/src/models/Favorite.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const favoriteSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+     property:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Property',
+        required: true
+    },
+})
