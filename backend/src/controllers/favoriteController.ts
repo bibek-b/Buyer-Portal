94bf778351f5216d546bfcd8fd2f9ea17a04281c
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 17:37:47 +0545

    favorite controller added

---
+++ backend/src/controllers/favoriteController.ts
@@ -0,0 +1,68 @@
+import type { Request, Response } from "express";
+import Favorite from "../models/Favorite.js";
+import Property from "../models/Property.js";
+import User from "../models/User.js";
+
+export const getAllFavorites = async (req: Request, res: Response) => {
+  try {
+    const favorites = await Favorite.find();
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "All favorites fetched successfully!",
+        data: favorites,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, messsage: "Server error. Please try again" });
+  }
+};
+
+export const addFavorite = async (req: Request, res: Response) => {
+  const userId = req.user.id;
+  const { propertyId } = req.params;
+  try {
+    const isPropertyExists = await Property.findById(propertyId);
+
+    if (!isPropertyExists) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Property doesnot exists!" });
+    }
+
+    const isUserExists = await User.findById(userId);
+
+    if (!isUserExists) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User doesnot exists!" });
+    }
+
+    await Favorite.create({ property: propertyId, user: userId });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, messsage: "Server error. Please try again" });
+  }
+};
+
+export const deleteFavorite = async (req: Request, res: Response) => {
+  const userId = req.user.id;
+  const { propertyId } = req.params;
+  try {
+    const isPropertyExists = await Property.findById(propertyId);
+
+    if (!isPropertyExists) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Property doesnot exists!" });
+    }
+    await Favorite.deleteOne({user: userId, property: propertyId})
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, messsage: "Server error. Please try again" });
+  }
+};
