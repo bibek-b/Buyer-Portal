dbce3a13ee5691c696dd33f644de82a62d7ef368
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-27 20:58:22 +0545

    auth constants & img added

---
+++ frontend/src/constants/authConstant.ts
@@ -0,0 +1,41 @@
+export const authFields = {
+  login: [
+    {
+      id: 2,
+      label: "Email",
+      inputType: "email",
+      placeholder: "jatt@gmail.com",
+    },
+    {
+      id: 3,
+      label: "Password",
+      inputType: "password",
+      placeholder: "•••••••••••",
+    },
+  ],
+  register: [
+    {
+      id: 1,
+      label: "Username",
+      placeholder: "Flying jatt",
+    },
+    {
+      id: 2,
+      label: "Email",
+      inputType: "email",
+      placeholder: "jatt@gmail.com",
+    },
+    {
+      id: 3,
+      label: "Password",
+      inputType: "password",
+      placeholder: "•••••••••••",
+    },
+    {
+      id: 4,
+      label: "Confirm Password",
+      inputType: "password",
+      placeholder: "•••••••••••",
+    },
+  ],
+};
