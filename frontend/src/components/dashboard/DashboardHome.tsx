1af7302ef08ab3fbaee269c9988b31a95ba8b1fb
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 21:41:58 +0545

    fetched all properties added

--- frontend/src/components/dashboard/DashboardHome.tsx
+++ frontend/src/components/dashboard/DashboardHome.tsx
@@ -1,16 +1,34 @@
+import { useEffect } from "react";
 import { usePropertyStore } from "../../stores/propertyStore";
 import { useUserStore } from "../../stores/userStore";
 import PropertyCard from "./PropertyCard";
+import { toast } from "react-toastify";
+import { propertyApi } from "../../api/propertyApi";
+import type { AxiosError } from "axios";
 
 const DashboardHome = () => {
-   const { properties } = usePropertyStore();
+  const { properties, setProperties } = usePropertyStore();
 
+  useEffect(() => {
+    const fetchAllProperties = async () => {
+      try {
+        const res = await propertyApi.getAllProperties();
+        setProperties(res.data.data);
+      } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        toast.error(err.response?.data?.message || "Something went wrong");
+      }
+    };
+    fetchAllProperties();
+  }, []);
 
-   const { user } = useUserStore();
+  const { user } = useUserStore();
 
   return (
     <div className="py-4 space-y-20">
-      <h2 className="text-4xl font-bold capitalize">Welcome 👋, {user.name} ({user.role})</h2>
+      <h2 className="text-4xl font-bold capitalize">
+        Welcome 👋, {user.name} ({user.role})
+      </h2>
 
       <span className=" absolute left-1/2 top-25 text-lg">
         List of available properties 🏘
