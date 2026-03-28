6b7b75564a4d9573524248317c291a3ceaf9cd3e
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 21:51:20 +0545

    minor changes

--- frontend/src/components/dashboard/LeftSidebar.tsx
+++ frontend/src/components/dashboard/LeftSidebar.tsx
@@ -6,16 +6,18 @@ const LeftSidebar = () => {
   console.log(currentUrlPath)
   return (
     <div className='bg-blue-500 w-[14%]  h-screen space-y-20 text-white  py-8 fixed'>
-       <div className="px-2 h-full flex flex-col items-center justify-between">
-         <h3 className="text-2xl font-bold">AppName 🏢</h3>
+       <div className="px-2 h-full flex flex-col text-center justify-between">
+         <div className="space-y-20">
+          <h3 className="text-2xl font-bold">AppName 🏢</h3>
 
-        <ul className="text-md w-full space-y-2 flex flex-col">
+        <ul className="text-md w-full space-y-6 flex flex-col">
            {leftSidebarOptions.map(opt => (
              <Link to={opt.path} key={opt.id} className={`hover:bg-black/10 p-2 rounded-3xl cursor-pointer ${currentUrlPath === opt.path && "bg-black/10"}`}>
                 {opt.label}
             </Link>
            ))}
         </ul>
+         </div>
 
         <button
         className="bg-red-500 w-full  py-1 rounded-full px-6 cursor-pointer hover:bg-red-600 transition-colors duration-300">Logout</button>
