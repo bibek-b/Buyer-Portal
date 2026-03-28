47312711c4bd14997ec91ac7d1fac85190263686
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 18:13:41 +0545

    set storage data of user

--- frontend/src/components/dashboard/DashboardHome.tsx
+++ frontend/src/components/dashboard/DashboardHome.tsx
@@ -1,11 +1,15 @@
 import { usePropertyStore } from "../../stores/propertyStore";
+import { useUserStore } from "../../stores/userStore";
 import PropertyCard from "./PropertyCard";
 
 const DashboardHome = () => {
    const { properties } = usePropertyStore();
+
+   const { user } = useUserStore();
+
   return (
     <div className="py-4 space-y-20">
-      <h2 className="text-4xl font-bold">Welcome 👋, Bibek (Buyer)</h2>
+      <h2 className="text-4xl font-bold capitalize">Welcome 👋, {user.name} ({user.role})</h2>
 
       <span className=" absolute left-1/2 top-25 text-lg">
         List of available properties 🏘
--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -4,7 +4,7 @@ import { storage } from "../utils/storage";
 
 const STORAGE_KEY = "user";
 export const useUserStore = create<UserStoreType>((set) => ({
-  user: { _id: "", name: "", email: "", role: "" },
+  user: storage.get(STORAGE_KEY) || { _id: "", name: "", email: "", role: "" },
   setUser: (data: UserType) => {
     storage.set(STORAGE_KEY, data);
     set({ user: data });
