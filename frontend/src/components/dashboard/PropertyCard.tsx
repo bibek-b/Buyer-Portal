2026a76972350ad4765a2d48311f39e6d82516a3
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 12:59:12 +0545

    minor css changes

--- frontend/src/components/dashboard/PropertyCard.tsx
+++ frontend/src/components/dashboard/PropertyCard.tsx
@@ -22,7 +22,7 @@ const PropertyCard = ({ data }: { data: PropertyType[] }) => {
         data?.map((p) => (
           <div
             key={p.id}
-            className="w-72 min-h-80 shadow-lg flex flex-col  p-2 rounded-2xl gap-4 hover:scale-[1.05] transition-discrete duration-300"
+            className="w-72 min-h-80 shadow-xl shadow-indigo-500 flex flex-col  p-2 rounded-2xl gap-4 hover:scale-[1.05] transition-discrete duration-300"
           >
             <div className="relative w-full">
               <img src={img} className="w-full h-40 object-cover rounded-2xl" />
@@ -30,19 +30,19 @@ const PropertyCard = ({ data }: { data: PropertyType[] }) => {
                 {favorites.some((fav: favoriteType) => fav.id == p.id) ? (
                   <FaHeart
                     size={22}
-                    className="absolute top-2  right-4 text-blue-500 cursor-pointer"
+                    className="absolute top-2  right-4 text-blue-500 cursor-pointer hover:scale-[1.2] transition-discrete duration-300 ease-in-out"
                   />
                 ) : (
                   <CiHeart
                     size={22}
-                    className="absolute top-2  right-4 text-white cursor-pointer"
+                    className="absolute top-2  right-4 text-white cursor-pointer hover:scale-[1.2] "
                   />
                 )}
               </div>
             </div>
 
-            <div className="space-y-2 text-black/80 flex flex-col  justify-center">
-              <span className="text-xl font-semibold ">Rs.{p.price}</span>
+            <div className="space-y-2 text-black/80 flex flex-col  justify-center h-full">
+              <span className="text-xl font-semibold line-clamp-2">Rs.{p.price}</span>
               <h3 className="font-medium text-2xl">{p.title}</h3>
 
               <span className="italic">{p.location}</span>
