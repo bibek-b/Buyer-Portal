66fc41ec9207bccace0165b50195221f07e614c2
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 08:09:13 +0545

    constants added for left side bar options

--- frontend/src/components/LeftSidebar.tsx
+++ frontend/src/components/LeftSidebar.tsx
@@ -1,3 +1,4 @@
+import { leftSidebarOptions } from "../constants/dashboardConstant"
 
 const LeftSidebar = () => {
   return (
@@ -5,10 +6,12 @@ const LeftSidebar = () => {
        <div className="px-2 h-full flex flex-col items-center justify-between">
          <h3 className="text-2xl font-bold">AppName 🏢</h3>
 
-        <ul className="text-lg w-full">
-            <li className="hover:bg-black/10 p-2 rounded-3xl cursor-pointer">
-                 My Favorites 💜
+        <ul className="text-md w-full space-y-2">
+           {leftSidebarOptions.map(opt => (
+             <li key={opt.id} className="hover:bg-black/10 p-2 rounded-3xl cursor-pointer">
+                {opt.label}
             </li>
+           ))}
         </ul>
 
         <button
---
+++ frontend/src/constants/dashboardConstant.ts
@@ -0,0 +1,6 @@
+export const leftSidebarOptions = [
+    {id: 1, label: "Properities 🏘", accessor: "properties"},
+    {id: 2, label: "My Favorites 💜", accessor: "myFavorites"},
+    {id: 3, label: "Settings ⚙️", accessor: "settings"},
+    
+]
