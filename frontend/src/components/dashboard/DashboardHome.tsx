0815ea274b4af04e3bee977cf8517684718d68ea
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 12:41:49 +0545

    emoji added on welcome mssg

--- frontend/src/components/dashboard/DashboardHome.tsx
+++ frontend/src/components/dashboard/DashboardHome.tsx
@@ -5,7 +5,7 @@ const DashboardHome = () => {
    const { properties } = usePropertyStore();
   return (
     <div className="py-4 space-y-20">
-      <h2 className="text-4xl font-bold">Welcome, Bibek (Buyer)</h2>
+      <h2 className="text-4xl font-bold">Welcome 👋, Bibek (Buyer)</h2>
 
       <span className=" absolute left-1/2 top-25 text-lg">
         List of available properties 🏘
