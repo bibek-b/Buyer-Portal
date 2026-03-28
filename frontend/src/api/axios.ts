c5cd8e05ad118dfb78b7d58cb4d8c4c2016d2cc1
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 21:40:15 +0545

    cookie allow and 401 redirect handled

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,22 @@
 import axios from "axios";
 import { env } from "../config/env";
+import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: env.serverUrl
+    baseURL: env.serverUrl,
+    withCredentials: true
 });
+
+api.interceptors.response.use(response => response, (error) => {
+    if(error.response.status === 401){
+        toast.error(error.response.data.message);
+
+        window.location.href ="/auth/login"
+    }
+    return Promise.reject(error);
+} )
+
+
 
 
 export default api;
