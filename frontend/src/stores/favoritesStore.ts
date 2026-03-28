1c53e3e42c4c2db515ced1f29e6b95ea134fbb6c
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-28 10:05:01 +0545

    storage functionality added to favorite store

--- frontend/src/stores/favoritesStore.ts
+++ frontend/src/stores/favoritesStore.ts
@@ -1,9 +1,25 @@
 import { create } from "zustand";
 import type { FavoriteStoreType, favoriteType } from "../types/favoriteType";
+import { storage } from "../utils/storage";
 
-export const useFavoriteStore = create<FavoriteStoreType>(set => ({
-    favorites: [],
-    setFavorites: (data:favoriteType[]) => set({favorites: data}),
-    addToFavorite: (data: favoriteType) => set(state => ({favorites: [...state.favorites, data]})),
-    removeFromFavorite: (id: string) => set(state => ({favorites: state.favorites.filter(f => f.id !== id)}))
-}))
+const STORAGE_KEY = "favorites";
+
+export const useFavoriteStore = create<FavoriteStoreType>((set) => ({
+  favorites: storage.get<favoriteType[]>(STORAGE_KEY) || [],
+  setFavorites: (data: favoriteType[]) => {
+    storage.set(STORAGE_KEY, data);
+    set({ favorites: data });
+  },
+  addToFavorite: (data: favoriteType) =>
+    set((state) => {
+      const updated = [...state.favorites, data];
+      storage.set(STORAGE_KEY, updated);
+      return { favorites: updated };
+    }),
+  removeFromFavorite: (id: string) =>
+    set((state) => {
+      const updated = state.favorites.filter((s) => s.id !== id);
+      storage.set(STORAGE_KEY, updated);
+      return { favorites: updated };
+    }),
+}));
