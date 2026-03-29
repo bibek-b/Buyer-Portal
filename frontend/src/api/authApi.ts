21c4081b3d700bf79309acf1c5816b7e54e5342c
Bibek Gadel <vieekgadel49@gmail.com>
2026-03-29 09:20:55 +0545

    logout added

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -4,4 +4,5 @@ import api from "./axios";
 export const authApi = { 
 register: (data: AuthFormDataType) => api.post('/auth/register', data),
 login: (data: AuthFormDataType) => api.post('/auth/login', data),
+logout: () => api.post('/auth/logout');
 }
